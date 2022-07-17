35071a12c83645050f07eaec0914390c0075b631
i-coder-robot <coder2998@gmail.com>
2022-07-17 21:21:36 +0800

    db

--- rust_pg_diesel_actix/src/db.rs
+++ rust_pg_diesel_actix/src/db.rs
@@ -4,30 +4,27 @@ use diesel::r2d2::ConnectionManager;
 use lazy_static::lazy_static;
 use r2d2;
 use std::env;
-use diesel_migrations::embed_migrations;
 
 type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 pub type DbConnection = r2d2::PooledConnection<ConnectionManager<PgConnection>>;
 
 embed_migrations!();
 
-
-lazy_static!{
-    static ref POOL:Pool = {
-        let db_url=env::var("DATABASE_URL").expect("数据库url没有设置");
+lazy_static! {
+    static ref POOL: Pool = {
+        let db_url = env::var("DATABASE_URL").expect("Database url not set");
         let manager = ConnectionManager::<PgConnection>::new(db_url);
-        Pool::new(manager).expect("数据库连接池失败")
+        Pool::new(manager).expect("Failed to create db pool")
     };
 }
 
-pub fn connection() -> Result<DbConnection,CustomError>{
-    POOL.get().map_err(|e| CustomError::new(500,
-        format!("获取数据库链接失败:{}",e)
-        ))
+pub fn init() {
+    lazy_static::initialize(&POOL);
+    let conn = connection().expect("Failed to get db connection");
+    embedded_migrations::run(&conn).unwrap();
 }
 
-pub fn init(){
-    lazy_static::initialize(&POOL);
-    let conn = connection().expect("获取数据库链接失败");
-    embedded_migrations::run(&conn).unwrap();
+pub fn connection() -> Result<DbConnection, CustomError> {
+    POOL.get()
+        .map_err(|e| CustomError::new(500, format!("Failed getting db connection: {}", e)))
 }
